403163a0999668cd591f0a64deb5e509c73b2ae9
Iker Ibarguren <ikerib@gmail.com>
2017-06-30 13:58:04 +0200

    fs api froga

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
+const dirTree = require('directory-tree');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,8 +15,14 @@ router.get('/servers', function (req, res, next) {
     const zerrenda = fs.readdirSync(srcpath)
         .filter(file => fs.lstatSync(path.join(srcpath, file)).isDirectory());
 
-    // return res.json(zerrenda);
     return res.send(200,zerrenda);
+
+});
+
+router.get('/ls', function (req, res, next) {
+    const srcpath = '/mnt/nfs/Aplik';
+    const tree = dirTree(srcpath);
+    return res.json( tree);
 
 });
 
